c6c2024a40de7c41e21ecd7e84e3d5cac2f843ab
Dmytro Teren <leo20091986@gmail.com>
2025-05-10 15:54:10 +0300

    initial commit

---
+++ src/task-5.ts
@@ -0,0 +1,15 @@
+function logStatus(status: Status): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+type Status = "loading" | "success" | "error";
+
+logStatus("loading");
+logStatus("success");
+logStatus("error");
